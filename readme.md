aa41016c1fcfe309d8ff7285280adb8393eec6ad
ON3JT <joost@hh31.be>
2021-02-27 09:39:08 +0100

    Update

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Introduction
-The idea comes from Pedro ON7WP whos was trying to use an older Icom transceiver for remote controle with Icom's RS-BA1 software.
+The idea comes from Pedro ON7WP who was trying to use an older Icom transceiver for remote controle with Icom's RS-BA1 software.
 The Icom IC275 is a VHF allmode rig, but lacks TX/RX switching via the CI-V commands.
 However, RS-BA1 sends the TX command regardless the transceiver. It uses the 0x00 addres (all transceivers) to send the TX commando.
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include <AltSoftSerial.h>
 
-// pin for PTT
-//#define PTT      (6)
+// pin for PTT, for test purposes use the builtin LED on port 13, change to your needs
+//#define PTT      (2)
 #define PTT (13)
+
 
 int CIV_SPEED = 9600;
 int RS232_SPEED = 9600;
@@ -12,17 +13,6 @@ AltSoftSerial CAT;      // RX, TX
 uint8_t read_buffer[8]; //Read buffer
 uint8_t counter = 0;
 
-/*
-Transmit command : 0x1C
-Sub              : 0x00
-TX on            : 0x01
-TX off           : 0x00
-
-Command from host to TRX would be :
-- TX on  : 0xFE 0xFE 0x50 0xE0 0x1C 0x00 0x01 0xFD
-- TX off : 0xFE 0xFE 0x50 0xE0 0x1C 0x00 0x00 0xFD
-Icom's RS-BA1 uses the 0x00 as to address
-*/
 
 void TX(bool Active)
 {
@@ -52,7 +42,7 @@ void loop()
   if (Serial.available())
   {
     sd_host = Serial.read();
-    if (byte(sd_host) == 0xFD) // if this is the end of the sentence, proces starts here
+    if (byte(sd_host) == 0xFD) // if this is the end of the serial sentence, processing starts here
     {
       read_buffer[counter] = 0xFD; // Add this to have a complete sentence
       if (read_buffer[0] == 0xFE && read_buffer[1] == 0XFE && read_buffer[3] == 0XE0)
